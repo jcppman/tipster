61e99fffce057437ce35284e03845c6b1ffcc06f
jcppman <jcppman@gmail.com>
2014-05-28 20:13:15 +0800

    init

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 tipster
 =======
 
-An activities manager for nodejs
+An activities manager for nodejs. Tipster will help you managing the ongoing 
+activities and help you find an available chance.
+
+Pretty abstract, hum?
+
+That's say that:
+
+Your awesome nodejs script will keep firing several type of async tasks
+concurrently, those tasks take times and resources, you want to limit the
+concurrency, but not only the concurrency of each task, you need to limit
+the overall concurrency also.
+
+More precisely:
+
+You have these tasks: A.a, A.b, B, C.a, C.z, D, you want to fire tasks with the
+given limitation:
+- max concurrency of A.a: 2
+- max concurrency of A.b: 1
+- max concurrency of B: 3
+- max concurrency of C.*: 5
+- max concurrency of *: 10
+
+__TO BE CONTINUE__
---
+++ index.js
@@ -0,0 +1,143 @@
+var underscore = require('underscore');
+
+function buildLooper (inItems) {
+
+  /*
+   * This function will return a funtion, which will throw out
+   * items circularly.
+   */
+
+  var items = inItems;
+  var now = 0;
+  var length = items.length;
+
+  return function () {
+
+    var item = items[now++];
+    now %= length;
+    return item;
+
+  };
+
+}
+
+function Tipster (params) {
+
+  var that = this;
+  var elements = params.elements;
+  var rules = params.rules;
+
+  that.elements = elements;
+  that.rules = rules;
+  that.pool = underscore.clone(rules);
+  that.looper = buildLooper(elements);
+  that.buildCheckers();
+
+}
+
+Tipster.prototype.buildCheckers = function () {
+
+  var that = this;
+  var elements = that.elements;
+  var rules = that.rules;
+  var pool = that.pool;
+  var logins = {};
+  var logouts = {};
+
+  elements.forEach(function (element) {
+
+    var related = underscore.keys(rules).filter(function (rule) {
+
+      if (rule.indexOf('*') !== -1) {
+
+        var reg = new RegExp(rule.replace('.','\\.').replace('*','.*'));
+        return (!!reg.exec(element));
+      
+      } else {
+
+        return (rule === element);
+      
+      }
+  
+    });
+
+    logins[element] = function () {
+
+      // The things checker do:
+      //   If all the related resources are available, occupy one of each
+      //   and return true, else return false
+
+      var rule;
+      for(var i = 0 ; i < related.length ; i++) {
+
+        rule = related[i];
+        if (pool[rule] <= 0) {
+
+          return false;
+        
+        }   
+      
+      }
+
+      related.forEach(function (target) {
+
+        pool[target]--;
+      
+      });
+
+      return true;
+
+    };
+
+    logouts[element] = function () {
+
+      // Release all related resources
+      related.forEach(function (rule) {
+
+        pool[rule]++;
+      
+      });
+    
+    };
+
+  });
+
+  that.logins = logins;
+  that.logouts = logouts;
+
+};
+
+Tipster.prototype.getTask = function () {
+
+  var that = this;
+  var logins = that.logins;
+  var max = that.elements.length;
+  var item;
+  var logout;
+
+  for (var i = 0 ; i < max ; i++) {
+
+    item = that.looper();
+    login = logins[item];
+    if (login()) {
+
+      return item;
+    
+    }  
+
+  }
+
+  return null;
+
+};
+
+Tipster.prototype.done = function (item) {
+
+  var that = this;
+  var logouts = that.logouts;
+  
+  logouts[item]();
+
+};
+
+module.exports = Tipster;
